d8b99d9d21572e7eab7ae27bc7a5db241f00664b
sianturiwilly <prajudiwilliam10@gmail.com>
2022-07-23 10:51:05 +0700

    Second commit

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Employee;
 use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
 {
-    //
+    public function index(){
+        
+        // return 'Sukses';
+        $data = Employee::all();
+        // dd($data);
+        return view('datapegawai', compact('data'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EmployeeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pegawai', [EmployeeController::class, 'index'])->name('pegawai');
