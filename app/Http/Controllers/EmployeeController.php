6cdbe073a83395e1f5fcdd35f5fbaf959adfbe69
sianturiwilly <prajudiwilliam10@gmail.com>
2023-02-19 22:11:47 +0700

    Commit kedua

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Employee;
 
 class EmployeeController extends Controller
 {
-    //
+    public function index(){
+
+        $data = Employee::all();
+        // dd($data);
+        return view('datapegawai',compact('data'));
+    }
 }
--- database/migrations/2023_02_19_140756_create_employees_table.php
+++ database/migrations/2023_02_19_140756_create_employees_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('nama');
             $table->enum('jeniskelamin',['pria','wanita']);
-            $table->integer('notelepon');
+            $table->bigInteger('notelepon');
             $table->timestamps();
         });
     }
